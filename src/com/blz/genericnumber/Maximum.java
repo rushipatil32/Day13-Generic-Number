1806532ac82591b288d6bc9a154ae7ce981ef7ac
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-26 22:49:29 +0530

    Added:find maximum number among the following

--- src/com/blz/genericnumber/Maximum.java
+++ src/com/blz/genericnumber/Maximum.java
@@ -1,33 +1,22 @@
 package com.blz.genericnumber;
 
-public class Maximum<T extends Comparable<T>> {
-	T a, b, c;
-
-	
-	public Maximum(T a, T b, T c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-
-	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
-		T max = a; 
-
-		if (b.compareTo(max) > 0) {
+public class Maximum {
+	public static Integer maxInteger(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0)
 			max = b;
-		}
-		if (c.compareTo(max) > 0) {
+		if (c.compareTo(max) > 0)
 			max = c;
-		}
-		return max; 
+
+		System.out.println("First Number= " + a);
+		System.out.println("Second Number= " + b);
+		System.out.println("Thrird Number= " + c);
+		System.out.println("Maximum Value is= " + max);
+		return max;
 	}
 
 	public static void main(String[] args) {
-		
-		System.out.printf("Max of %d, %d and %d is %d\n\n", 5, 6, 7, maximum(5, 6, 7));
-		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 5.7, 6.8, 7.9, maximum(5.7f, 6.8f, 7.9f));
-		System.out.printf("Max of %s, %s and %s is %s\n", "Mango", "orange", "Banana",
-				maximum("Mango", "orange", "Banana"));
+		Maximum.maxInteger(5, 6, 7);
 	}
 
 }
